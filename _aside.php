8a83c08503e29f196e9e48cf6df60fdcebe9ce26
Mael Michaud <mael.michaud.pro@gmail.com>
2025-01-09 16:19:24 +0100

    add _aside.php

---
+++ _aside.php
@@ -0,0 +1,7 @@
+<aside class="flex">
+    <h2 class="green-text aside-h2">Actualités :</h2>
+    <img class="aside-img" id="actu-img"src="img/actu.jpg" alt="Enfants arrosant des plantes dans un jardin">
+    <p class="aside-p"><a href="#">Plan national santé environnement et les plans réginaux santé environnement</a></p>
+    <img class="aside-img" id="ushuaia-img"src="img/ushuaia.png" alt="Logo fondation pour la nature et pour l'homme"/>
+    <img class="aside-img" id="wwf-img"src="img/wwf.jpg" alt="Logo wwf qui représente un panda"/>
+</aside>
