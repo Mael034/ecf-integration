5ec1e9abe269025e19aae02ec2ba9abed61c77fe
Mael Michaud <mael.michaud.pro@gmail.com>
2025-01-09 16:21:22 +0100

    add economie.php

---
+++ economie-energie.php
@@ -0,0 +1,126 @@
+<?php
+    require_once "_header.php";
+?>
+<main class="flex">
+    <div class="container">
+        <article>
+            <h1 class="article-h1 green-text">Économie d'énergie</h1>
+            <h2 class="article-h2 black-text"> Chauffage </h2>
+            <p class="main-p black-text">
+                C’est le plus gros poste de consommation (65% des dépenses d’énergie). Pour le réduire :
+            </p>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">J’évite de surchauffer. La température conseillée est 19°C dans le salon et la salle à manger et 16°C dans les chambres.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">J’éteins les radiateurs quand les fenêtres sont ouvertes pour aérer.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Lors de mon absence en journée, je laisse les volets et rideaux fermés pour conserver la chaleur et baisser le chauffage.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je baisse le chauffage dans les pièces inoccupées.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je purge régulièrement les radiateurs.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je fais entretenir tous les ans ma chaudière par un professionnel.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">J’installe un système de régulation et de programmation du chauffage.</p>
+                </li>
+            </ul>
+            <h2 class="article-h2 black-text"> Eau chaude </h2>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">Je règle la température de l’eau chaude sanitaire entre 55°C et 60°C (ce réglage permet également de supprimer le risque de brûlure).</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">J’isole les tuyaux et le ballon d’eau chaude.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">J’installe un mitigeur ou un robinet thermostatique.</p>
+                </li>
+            </ul>
+            <h2 class="article-h2 black-text"> Appareils électriques et éclairage </h2>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">J’éteins la lumière et les appareils électriques (TV, Hi-fi, ordinateur…) lorsque que je ne m’en sers pas.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">J’éteins les appareils plutôt que de les laisser en veille. Leur consommation en veille peut représenter jusqu’à 10% de ma facture d’électricité hors-chauffage.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je débranche les appareils électriques lorsque je ne les utilise pas : chargeur de portable, petit électroménager…</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je dépoussière les lampes et abat-jour pour une meilleure luminosité.</p>
+                </li>
+            </ul>
+            <h2 class="black-text">Electroménager</h2>
+            <h3 class="black-text">Réfrigérateur-congélateur :</h3>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">Je ne mets pas d’aliments encore chauds dans le congélateur ou le réfrigérateur.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je dépoussière les grilles arrière de ces appareils.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je dégivre dès que la couche de givre dépasse 3 mm.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je nettoie les joints des portes et je les change si besoin.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je ne place pas ces appareils près d’une source de chaleur (four ou fenêtre ensoleillée par exemple).</p>
+                </li>
+            </ul>
+            <h3 class="black-text">Lave-linge :</h3>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">Je lave mon linge à basse température</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je remplis entièrement mon lave-linge avant de le mettre en marche.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je fais sécher mon linge à l’air libre (le sèche-linge est très énergivore). Si ce n’est pas possible : j’essore bien le linge avant de le mettre dans le sèche-linge.</p>
+                </li>
+            </ul>
+            <h3 class="black-text">Lave-vaisselle :</h3>
+            <ul class="p-ul">
+                <li>
+                    <p class="main-p black-text">J’utilise la touche « Eco ». </p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je remplis entièrement le lave-vaisselle avant de le mettre en marche.</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je nettoie régulièrement les filtres et joints de porte.</p>
+                </li>
+            </ul>
+            <h3 class="black-text">Cuisson :</h3>
+            <ul>
+                <li>
+                    <p class="main-p black-text">Je couvre les casseroles lorsque je fais bouillir de l’eau (cela divise par 4 la consommation d’énergie).</p>
+                </li>
+                <li>
+                    <p class="main-p black-text">Je démarre un nettoyage par pyrolyse à la fin d’une cuisson pour profiter de la chaleur déjà produite.</p>
+                </li>
+            </ul>
+            <h3 class="black-text">Remplacement ou achat de nouveaux équipements :</h3>
+            <p class="main-p black-text">
+            L’installation d’équipements performants (pour le chauffage, l’électroménager, l’éclairage…), couplée à ces gestes, me permettra de réduire significativement ma consommation d’énergie.</p>
+           <img class="main-img" src="img/energie.jpg" alt="La planète Terre représentée dans une ampoule"/>
+        </article>
+        <?php
+            require_once "_aside.php";
+        ?>
+    </div>
+</main>
+<?php
+    require_once "_footer.php";
+?>
